1ea8079d4cacd65e4b26f4ac9f4d3996d5907fff
Jasser12544 <jasser.bawi19@gmail.com>
2023-01-26 11:40:12 +0000

    Initial commit of the base solution

---
+++ Selenium_Pom_Framework/Selenium_Pom_Framework/SL_POM_Framework_Should.cs
@@ -0,0 +1,12 @@
+using OpenQA.Selenium.Chrome;
+
+namespace SL_Pom_Framework_Test;
+
+public class SL_POM_Framework_Should
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
