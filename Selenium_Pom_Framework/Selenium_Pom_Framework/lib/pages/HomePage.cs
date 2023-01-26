94d326b38509ba64fb35116c27c2c07ade821165
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-26 12:26:17 +0000

    Added structure for the tests

---
+++ Selenium_Pom_Framework/Selenium_Pom_Framework/lib/pages/HomePage.cs
@@ -0,0 +1,24 @@
+﻿namespace SL_Pom_Framework_Test.lib.pages;
+
+public class HomePage
+{
+    public void VisitHomePage()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void ClickLoginButton()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void EnterPassword(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void EnterUserName(string userName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Selenium_Pom_Framework/Selenium_Pom_Framework/lib/pages/SL_Website.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace SL_Pom_Framework_Test.lib.pages;
+
+public class SL_Website<IWebDriver>
+{
+    public IWebDriver SeleniumDriver { get; internal set; }
+    public HomePage SL_HomePage { get; internal set; }
+
+    public SL_Website()
+    {
+    }
+
+}
